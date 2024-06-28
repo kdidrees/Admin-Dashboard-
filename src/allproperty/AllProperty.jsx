e54e1501587beca2143a66b4432f2b6636629c6b
khurshid <Khurshid@qurilo.com>
2024-06-28 14:51:48 +0530

    update in allproperty

--- src/allproperty/AllProperty.jsx
+++ src/allproperty/AllProperty.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { CiLocationOn } from "react-icons/ci";
-
+import { FaHouseChimneyWindow } from "react-icons/fa6";
+import { SlCalender } from "react-icons/sl";
+import { TbRulerMeasure } from "react-icons/tb";
 
 export default function AllProperty() {
   return (
@@ -22,21 +24,53 @@ export default function AllProperty() {
               </h2>
 
               <div className="flex">
-              <span className="mt-3"><CiLocationOn style={{fontSize:'1.7rem'}} /></span>
-                <p className="ml-2 font-sans md:text-xl text-base mt-3 text-gray-500"> Richmond Place 1312, Charlotte, New York</p>
+                <span className="mt-3">
+                  <CiLocationOn style={{ fontSize: "1.7rem" }} />
+                </span>
+                <p className="ml-2 font-sans md:text-xl text-base mt-3 text-gray-500">
+                  {" "}
+                  Richmond Place 1312, Charlotte, New York
+                </p>
               </div>
             </div>
-            <div className="flex justify-between flex-col">
-              <div className="mr-4 mt-2">
-                <p className="font-sans md:text-xl text-base"> Apartment <span>| by Robin johnson</span> </p>
-               
-              </div>
+            <div className="flex justify-between  flex-col">
               <div className="mt-2 flex flex-row">
-                <p>Мод: Zombie Mod</p>
-                <p>Карта: cs_italy</p>
-                <p>Игроки: 24/32</p>
+                <div class="flex items-center gap-4 mx-8">
+                  <FaHouseChimneyWindow style={{ fontSize: "1.7rem" }}/>
+                  <div>
+                    <p class="block antialiased font-sans text-base font-light leading-relaxed text-blue-gray-900 mb-0.5 !font-semibold">
+                      Single Family
+                    </p>
+                    <p class="block antialiased font-sans text-sm leading-normal text-gray-700 font-normal">
+                      property type
+                    </p>
+                  </div>
+                </div>
+
+                <div class="flex items-center gap-4 mx-8">
+                 <SlCalender style={{ fontSize: "1.7rem" }}/>
+                  <div>
+                    <p class="block antialiased font-sans text-base font-light leading-relaxed text-blue-gray-900 mb-0.5 !font-semibold">
+                      6 days
+                    </p>
+                    <p class="block antialiased font-sans text-sm leading-normal text-gray-700 font-normal">
+                    Time on Realtor.com
+                    </p>
+                  </div>
+                </div>
+
+                <div class="flex items-center gap-4 mx-8">
+                <TbRulerMeasure style={{ fontSize: "1.7rem" }}/>
+                  <div>
+                    <p class="block antialiased font-sans text-base font-light leading-relaxed text-blue-gray-900 mb-0.5 !font-semibold">
+                      John Doe
+                    </p>
+                    <p class="block antialiased font-sans text-sm leading-normal text-gray-700 font-normal">
+                      2022-08-15
+                    </p>
+                  </div>
+                </div>
               </div>
-             
             </div>
           </div>
         </div>
