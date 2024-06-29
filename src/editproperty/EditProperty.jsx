5aba8860cf074f5424724524718cd8f7881331cf
khurshid <Khurshid@qurilo.com>
2024-06-29 11:08:26 +0530

    editproperty component ui updated

--- src/editproperty/EditProperty.jsx
+++ src/editproperty/EditProperty.jsx
@@ -1,168 +1,277 @@
 import React from "react";
-import { useForm } from "react-hook-form";
-import { useNavigate } from "react-router-dom";
-import { useDispatch } from "react-redux";
-import { createProperty } from "../redux/slices/propertySlice";
 
 export default function EditProperty() {
-  const { register, handleSubmit } = useForm();
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-
-  const onSubmit = (data) => {
-    console.log(data,'ye hi h bro');
-
-    // Create a FormData object to handle file uploads
-    const formData = new FormData();
-    for (let key in data) {
-      if (key === 'file') {
-        for (let i = 0; i < data[key].length; i++) {
-          formData.append('images', data[key][i]);
-        }
-      } else {
-        formData.append(key, data[key]);
-      }
-    }
-
-    // Dispatch createProperty asyncThunk
-    dispatch(createProperty(formData))
-      .then((response) => {
-        if (response.meta.requestStatus === 'fulfilled') {
-          navigate('/all-properties');
-        }
-      })
-      .catch((error) => {
-        console.error('Error uploading property:', error);
-      });
-  };
-
   return (
     <>
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="editor mx-auto w-10/12 mt-44 flex flex-col text-gray-800 border p-4 shadow-lg max-w-5xl"
-      >
-        <h1 className="heading text-center font-bold text-3xl m-5 pb-6 text-black">
-          Create New Property Listing
-        </h1>
-
-        <input
-          {...register("price", { required: true })}
-          className="bg-gray-100 border md:text-xl border-gray-300 p-4 mb-4 outline-none"
-          placeholder="Price"
-          type="number"
-          required
+      <>
+        {/* component */}
+        <link
+          rel="stylesheet"
+          href="https://demos.creative-tim.com/notus-js/assets/styles/tailwind.css"
         />
-
-        <div className="flex justify-between">
-          <input
-            {...register("bedrooms", { required: true })}
-            className="title bg-gray-100 border md:text-xl border-gray-300 p-4 mb-4 outline-none"
-            placeholder="Bedrooms"
-            type="number"
-            required
-          />
-
-          <input
-            {...register("bathrooms", { required: true })}
-            className="title bg-gray-100 border md:text-xl border-gray-300 p-4 mb-4 outline-none"
-            placeholder="Bathrooms"
-            type="number"
-            required
-          />
-
-          <input
-            {...register("area", { required: true })}
-            className="title bg-gray-100 border md:text-xl border-gray-300 p-4 mb-4 outline-none"
-            placeholder="Area in sqft"
-            type="number"
-            required
-          />
-        </div>
-
-        <input
-          {...register("address", { required: true })}
-          className="bg-gray-100 border md:text-xl border-gray-300 p-4 mb-4 outline-none"
-          placeholder="Address"
-          type="text"
-          required
+        <link
+          rel="stylesheet"
+          href="https://demos.creative-tim.com/notus-js/assets/vendor/@fortawesome/fontawesome-free/css/all.min.css"
         />
-
-        <div className="flex justify-between">
-          <input
-            {...register("lotSize", { required: true })}
-            className="title bg-gray-100 border md:text-xl border-gray-300 p-4 mb-4 outline-none"
-            placeholder="Lot size"
-            type="number"
-            step={'any'}
-            required
-          />
-
-          <input
-            {...register("propertyType", { required: true })}
-            className="title bg-gray-100 border md:text-xl border-gray-300 p-4 mb-4 outline-none"
-            placeholder="Property type"
-            type="text"
-            required
-          />
-
-          <input
-            {...register("daysOnRealtor", { required: true })}
-            className="title bg-gray-100 border md:text-xl border-gray-300 p-4 mb-4 outline-none"
-            placeholder="Days on realtor"
-            type="number"
-            required
-          />
-        </div>
-
-        <div className="flex justify-between">
-          <input
-            {...register("pricePerSqft", { required: true })}
-            className="title bg-gray-100 border md:text-xl border-gray-300 p-4 mb-4 outline-none"
-            placeholder="Price per sqft"
-            type="number"
-            required
-          />
-
-          <input
-            {...register("yearBuilt", { required: true })}
-            className="title bg-gray-100 border md:text-xl border-gray-300 p-4 mb-4 outline-none"
-            placeholder="Year built"
-            type="number"
-            required
-          />
-
-          <input
-            {...register("garage", { required: true })}
-            className="title bg-gray-100 border md:text-xl border-gray-300 p-4 mb-4 outline-none"
-            placeholder="Garages"
-            type="number"
-            required
-          />
-        </div>
-
-        <textarea
-          {...register("description", { required: true })}
-          className="description bg-gray-100 md:text-xl sec p-3 h-60 border border-gray-300 outline-none"
-          placeholder="Describe everything about this post here"
-          required
-        />
-
-        <input
-          {...register("file", { required: true })}
-          className="mt-4 title bg-gray-100 border md:text-xl border-gray-300 p-4 mb-4 outline-none"
-          type="file"
-          multiple
-          required
-        />
-
-        <button
-          type="submit"
-          className="btn border border-indigo-500 md:text-xl p-4 px-4 font-semibold cursor-pointer text-gray-200 ml-2 bg-indigo-500"
-        >
-          Create 
-        </button>
-      </form>
+        <section className=" py-1 bg-blueGray-50 mt-24">
+          <div className="w-full lg:w-8/12 px-4 mx-auto mt-6">
+            <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-100 border-0">
+              <div className="rounded-t bg-white mb-0 px-6 py-6">
+                <div className="text-center flex justify-between">
+                  <h6 className="text-blueGray-700 text-xl font-bold">
+                    Create New Property{" "}
+                  </h6>
+                  <button
+                    className="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150"
+                    type="button"
+                  >
+                    Settings
+                  </button>
+                </div>
+              </div>
+              <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
+                <form>
+                  <h6 className="text-blueGray-400 text-sm mt-3 mb-6 font-bold uppercase">
+                    Property Information
+                  </h6>
+                  <div className="flex flex-wrap">
+                    <div className="w-full lg:w-6/12 px-4">
+                      <div className="relative w-full mb-3">
+                        <label
+                          className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                          htmlfor="grid-password"
+                        >
+                          price
+                        </label>
+                        <input
+                          type="text"
+                          className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                        />
+                      </div>
+                    </div>
+                    <div className="w-full lg:w-6/12 px-4">
+                      <div className="relative w-full mb-3">
+                        <label
+                          className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                          htmlfor="grid-password"
+                        >
+                          Bedrooms
+                        </label>
+                        <input
+                          type="text"
+                          className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                        />
+                      </div>
+                    </div>
+                    <div className="w-full lg:w-6/12 px-4">
+                      <div className="relative w-full mb-3">
+                        <label
+                          className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                          htmlfor="grid-password"
+                        >
+                          Bathrooms
+                        </label>
+                        <input
+                          type="text"
+                          className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                        />
+                      </div>
+                    </div>
+                    <div className="w-full lg:w-6/12 px-4">
+                      <div className="relative w-full mb-3">
+                        <label
+                          className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                          htmlfor="grid-password"
+                        >
+                          Area in sqft
+                        </label>
+                        <input
+                          type="text"
+                          className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <hr className="mt-6 border-b-1 border-blueGray-300" />
+                  {/* <h6 className="text-blueGray-400 text-sm mt-3 mb-6 font-bold uppercase">
+              Contact Information
+            </h6> */}
+                  <div className="flex flex-wrap">
+                    <div className="w-full lg:w-12/12 px-4">
+                      <div className="relative w-full mb-3">
+                        <label
+                          className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                          htmlfor="grid-password"
+                        >
+                          Address
+                        </label>
+                        <input
+                          type="text"
+                          className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                          // defaultValue="Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09"
+                        />
+                      </div>
+                    </div>
+                    <div className="w-full lg:w-4/12 px-4">
+                      <div className="relative w-full mb-3">
+                        <label
+                          className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                          htmlfor="grid-password"
+                        >
+                          Lot Size
+                        </label>
+                        <input
+                          type="text"
+                          className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                        />
+                      </div>
+                    </div>
+                    <div className="w-full lg:w-4/12 px-4">
+                      <div className="relative w-full mb-3">
+                        <label
+                          className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                          htmlfor="grid-password"
+                        >
+                          Property Type
+                        </label>
+                        <input
+                          type="text"
+                          className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                        />
+                      </div>
+                    </div>
+                    <div className="w-full lg:w-4/12 px-4">
+                      <div className="relative w-full mb-3">
+                        <label
+                          className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                          htmlfor="grid-password"
+                        >
+                          Days on realtor
+                        </label>
+                        <input
+                          type="text"
+                          className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                        />
+                      </div>
+                    </div>
+                    <div className="w-full lg:w-4/12 px-4">
+                      <div className="relative w-full mb-3">
+                        <label
+                          className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                          htmlfor="grid-password"
+                        >
+                          price per sqft
+                        </label>
+                        <input
+                          type="text"
+                          className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                        />
+                      </div>
+                    </div>
+                    <div className="w-full lg:w-4/12 px-4">
+                      <div className="relative w-full mb-3">
+                        <label
+                          className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                          htmlfor="grid-password"
+                        >
+                          year built
+                        </label>
+                        <input
+                          type="text"
+                          className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                        />
+                      </div>
+                    </div>
+                    <div className="w-full lg:w-4/12 px-4">
+                      <div className="relative w-full mb-3">
+                        <label
+                          className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                          htmlfor="grid-password"
+                        >
+                          garages
+                        </label>
+                        <input
+                          type="text"
+                          className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <hr className="mt-6 border-b-1 border-blueGray-300" />
+                  <h6 className="text-blueGray-400 text-sm mt-3 mb-6 font-bold uppercase">
+                    property description
+                  </h6>
+                  <div className="flex flex-wrap">
+                    <div className="w-full lg:w-12/12 px-4">
+                      <div className="relative w-full mb-3">
+                        {/* <label
+                    className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                    htmlfor="grid-password"
+                  >
+                    About me
+                  </label> */}
+                        <textarea
+                          type="text"
+                          className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                          rows={4}
+                        />
+                        <div className="mt-6">
+                          <label
+                            className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                            htmlfor="grid-password"
+                          >
+                            About me
+                          </label>
+                          <input
+                            className="mt-4 title bg-gray-100 border md:text-xl border-gray-300 p-4 mb-4 outline-none"
+                            type="file"
+                            multiple
+                            required
+                          />
+                        </div>
+                        <button
+                          type="submit"
+                          className="btn border border-indigo-500 md:text-xl p-4 px-4 font-semibold cursor-pointer text-gray-200 ml-2 bg-indigo-500"
+                        >
+                          Create
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+            <footer className="relative  pt-8 pb-6 mt-2">
+              <div className="container mx-auto px-4">
+                <div className="flex flex-wrap items-center md:justify-between justify-center">
+                  <div className="w-full md:w-6/12 px-4 mx-auto text-center">
+                    <div className="text-sm text-blueGray-500 font-semibold py-1">
+                      Made with{" "}
+                      <a
+                        href="https://www.creative-tim.com/product/notus-js"
+                        className="text-blueGray-500 hover:text-gray-800"
+                        target="_blank"
+                      >
+                        Notus JS
+                      </a>{" "}
+                      by{" "}
+                      <a
+                        href="https://www.creative-tim.com"
+                        className="text-blueGray-500 hover:text-blueGray-800"
+                        target="_blank"
+                      >
+                        {" "}
+                        Creative Tim
+                      </a>
+                      .
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </footer>
+          </div>
+        </section>
+      </>
     </>
   );
 }
